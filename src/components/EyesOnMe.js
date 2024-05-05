e1b24433b0005f429a9db1df01599c84e7eda69e
Faith <loshyfaith@gmail.com>
2024-05-05 20:43:43 +0300

    event handling

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+
+  return (
+    <button 
+    onFocus={(a)=> {console.log("Good!")}}
+    onBlur={(a)=> {console.log("Hey! Eyes on me!")}}
+    >Eyes on me</button>
+  )
+
+}
+export default EyesOnMe
